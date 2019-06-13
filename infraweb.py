d12f159559475c14e326dd8631894c765a128c48
root <root@ansible.localdomain>
2019-06-13 10:32:42 +0200

    InfraChange

--- infraweb.py
+++ infraweb.py
@@ -17,26 +17,36 @@ def get_args():
 
 	return args
 
+def deployweb():
+	deploy1 = VMwareMod("web1","a")
+	deploy2 = VMwareMod("web2","b")
+	os.system('ansible-playbook Ansible/newvmweb.yml')
+#	vlan1 = VMwareVlan("web1")
+#	vlan1 = VMwareVlan("web2")
+
+def deploydns():
+	deploy1 = VMwareMod("dns1","c")
+	deploy2 = VMwareMod("dns2","d")
+	os.system('chmod 400 Cert/id_rsa-pa')
+	os.system('ansible-playbook Ansible/newvmdns.yml')
+
+def appweb
+	time.sleep(70)
+	os.system('ansible-playbook Ansible/infra_web_docker.yml')
+
+def appdns():
+	time.sleep(70)
+	os.system('ansible-playbook Ansible/infra_web_docker.yml')
+
 def main():
 
 	args = get_args()
 	if args.all:
-		deploy1 = VMwareMod("web1","a")
-		deploy2 = VMwareMod("web2","b")
-		os.system('chmod 400 Cert/id_rsa-pa')
-		os.system('ansible-playbook Ansible/newvmweb.yml')
-
-		deploy1 = VMwareMod("dns1","c")
-		deploy2 = VMwareMod("dns2","d")
-		os.system('ansible-playbook Ansible/newvmdns.yml')
-
-#		vlan1 = VMwareVlan("web1")
-#		vlan1 = VMwareVlan("web2")
-
 		os.system('cp Ansible/conf/hosts /etc/ansible/hosts')
-#		time.sleep(70)
-#		os.system('ansible-playbook Ansible/infra_web_docker.yml')
-		os.system('ansible-playbook Ansible/infra_dns.yml')
+		deployweb()
+		deploydns()
+		appweb()
+		appdns()
 
 if __name__ == "__main__":
     main()
